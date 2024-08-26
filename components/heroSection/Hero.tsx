74f82258590a495a6371032b7611635d87ce45ef
Satyansh Mittal <mittalsatyansh@gmail.com>
2024-08-27 01:11:49 +0530

    logo created

---
+++ components/heroSection/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from "./RoundLogo"
+
+export default function hero() {
+  return (
+    <div className='w-full h-screen flex flex-col justify-center text-white items-center'>
+    <Logo/>    
+    
+
+        
+    </div>
+  )
+}
